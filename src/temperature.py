39067fb2d319b7ec9acc3823b8f7d7e6afa2a015
CK Lee <cheekong@mit.edu>
2015-06-23 10:18:01 -0400

    Reverted back to f_to_k

--- src/temperature.py
+++ src/temperature.py
@@ -4,18 +4,8 @@
 """
 
 def f_to_k(temp):
-    converted = ((temp-32.0)*(5.0/9.0)) + 273.15
+    converted = ((temp-32)*(5/9)) + 273.15
     return converted
-
-def k_to_c(temp):
-    return temp - 273.15
-
-def f_to_c(temp):
-    temp_k = f_to_k(temp)
-    temp_c = k_to_c(temp_k)
-    return temp_c 
 
 print(f_to_k(32))
 print(f_to_k(212))
-print(k_to_c(273.15))
-print(f_to_c(32.0))
